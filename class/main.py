986681c423fa4cb6504148440244e981267f1a0f
Thirumalesh678 <nama.thirumalesh@gmail.com>
2025-06-13 16:06:29 +0530

    update the classes and organize

--- class/main.py
+++ class/main.py
@@ -1,16 +1,47 @@
 from Employee import Employee
 
+#Initializing all the employee in our org
 employees= {
-    401:  Employee('thiru',678,'A1',2500000),
-    402:  Employee('chinna',679,'A1',1500000),
-    403:  Employee('sai',675,'A1',4500000)
+    675:  Employee('thiru',675,'A1',2500000),
+    678:  Employee('chinna',678,'A1',1500000),
+    679:  Employee('sai',679,'A1',4500000)
 }
+
+def totalpayroll(employees):
+    totalsalary = 0
+    for emp in employees.values():
+        totalsalary += emp.salary
+    print(totalsalary)
+
+
+
+#Displaying all the employee in our org
 for emp_id,emp  in employees.items():
     e = emp.showEmployee()
-    print(e)
+    print("\n")
+
+#Bonues for all employee in our org
 for emp_id,emp in employees.items():
+    print(emp.name +"'s bonus is: ",emp.salary)
     bouns = emp.calculatebouns(0.12)
-    print(emp.name +"'s bouns is: ",bouns)
+    print(emp.name +"'s bonus is: ",bouns)
+print("\n")
+
+employees[678].updateSalary(2500000)
+print(employees[678].name+"'s bouns is: ",employees[678].salary)
+print("\n")
+
+
+print("bonus after salary incremental \n")
+for emp_id,emp in employees.items():
+    print(emp.name +"'s bonus is: ",emp.salary)
+    bouns = emp.calculatebouns(0.12)
+    print(emp.name +"'s bonus is: ",bouns)
+
+print("\n")
+
+totalpayroll(employees)
+
 
 
 
