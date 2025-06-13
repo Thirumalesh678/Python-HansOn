be88459fce3768b401481410fe534644ad230842
Thirumalesh678 <nama.thirumalesh@gmail.com>
2025-06-13 16:36:10 +0530

    added find employee function

--- class/main.py
+++ class/main.py
@@ -6,13 +6,26 @@ employees= {
     678:  Employee('chinna',678,'A1',1500000),
     679:  Employee('sai',679,'A1',4500000)
 }
-
+#Display total payroll for our org
 def totalpayroll(employees):
     totalsalary = 0
     for emp in employees.values():
         totalsalary += emp.salary
     print(totalsalary)
 
+#Display Bonus and salary for all the employee in our org
+def showBonusAndSalary():
+    for emp_id,emp in employees.items():
+         print(emp.name +"'s bonus is: ",emp.salary)
+         bouns = emp.calculatebouns(0.12)
+         print(emp.name +"'s bonus is: ",bouns)
+
+#Finding employee from the collection
+def findTheEmployee(empId):
+    if(employees.get(empId)):
+        return employees.get(empId)
+    else:
+        return []
 
 
 #Displaying all the employee in our org
@@ -20,28 +33,28 @@ for emp_id,emp  in employees.items():
     e = emp.showEmployee()
     print("\n")
 
-#Bonues for all employee in our org
-for emp_id,emp in employees.items():
-    print(emp.name +"'s bonus is: ",emp.salary)
-    bouns = emp.calculatebouns(0.12)
-    print(emp.name +"'s bonus is: ",bouns)
-print("\n")
 
+showBonusAndSalary()
+print("\n")
+#calibration for employee having less ctc 
 employees[678].updateSalary(2500000)
 print(employees[678].name+"'s bouns is: ",employees[678].salary)
 print("\n")
 
-
+#Bonues for all the employee in our org 
 print("bonus after salary incremental \n")
-for emp_id,emp in employees.items():
-    print(emp.name +"'s bonus is: ",emp.salary)
-    bouns = emp.calculatebouns(0.12)
-    print(emp.name +"'s bonus is: ",bouns)
+showBonusAndSalary()
 
 print("\n")
-
+#Display total payroll for our org
 totalpayroll(employees)
 
+eId= int(input("input the employee id to find"))
+empl =findTheEmployee(eId)
+if empl!=[]:
+    empl.showEmployee()
+else:
+    print("Employee not found!")
 
 
 
